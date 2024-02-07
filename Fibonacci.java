13ed46be26002acf344771776d32359d180270fa
Sangeetha M <01fe22bca179@kletech.ac.in>
2024-02-07 06:26:26 +0530

    Code for fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci series: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+          
+        }
+        
+    }
+
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+            
+        }
+    }
+
+    
+}
